8c8861e5b0ce83c9b59ec6300cc1c7f98770d361
Giulio Zorzi <giulio.zorzi.89@gmail.com>
2022-06-22 16:32:49 +0200

    migliorato

--- Inserimento_anagrafica.py
+++ Inserimento_anagrafica.py
@@ -1,5 +1,7 @@
 from Conferma_salvataggio import salva
 from Mostra_lista import lista
+from datetime import date 
+
 
 def inserimento_anagrafica(df_anagrafica, FILE_PATH):
     inserimento = []
@@ -7,7 +9,11 @@ def inserimento_anagrafica(df_anagrafica, FILE_PATH):
     codice = int(antecedente) + 1
     inserimento.append(codice)
     for i in range(1,len(lista())):
-        dato = input(f"Inserisci {lista()[i]}").upper()
-        inserimento.append(dato)
+        if lista()[i] == "DATAREG":
+            data_corrente = date.today()
+            inserimento.append(data_corrente)
+        else:    
+            dato = input(f"Inserisci {lista()[i]}").upper()
+            inserimento.append(dato)
     df_anagrafica.loc[len(df_anagrafica)] = inserimento
     salva(df_anagrafica,FILE_PATH)
